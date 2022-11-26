f56fbc2edcb11ac98faea3ca3f6fa4685eb0149f
ThomaInasaridze <115581820+ThomaInasaridze@users.noreply.github.com>
2022-11-26 10:44:33 +0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// // Task1
+
+// const a = prompt ('Enter the First digit :')
+// const b = prompt ('Enter the Second digit :')
+// console.log("The sum of the entered numbers is : "+ (Number(a)+Number(b)))
+// console.log("The product of the entered numbers is : "+ (Number(a)*Number(b)))
+
+
+
+// // Task2
+
+
+// let a = prompt("Enter the First digit: ")
+
+// let b = prompt("Enter the Second digit : ")
+
+// let c = prompt("Enter the Third digit : ")
+
+// let d = prompt("Enter the Fourth digit : ")
+
+// if(a > b && a > c && a > d) {
+//     console.log(a)
+// }
+// if(b > a && b > c && b > d) {
+//     console.log(b)
+// }
+// if(c > a && c > b && c > d ) {
+//     console.log(c)
+// }
+// else{
+//     console.log ( "The biggest of the entered numbers is: " + (d))
+// }
+
+
+// // Task3
+
+// const num = prompt("Enter the Numbers")
+
+// if ((num%2)==1) {
+//     console.log("The number is odd")
+// }
+
+// else {
+//     console.log("The number is even")
+// }
